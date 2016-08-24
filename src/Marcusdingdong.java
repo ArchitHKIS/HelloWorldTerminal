4a2725937cd37a0fc77a9aa6653a91b132b8cb3c
ArchitHKIS <190902@hkis.edu.hk>
2016-08-24 08:02:05 +0800

    Init
    Init project

---
+++ src/Marcusdingdong.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Marcusdingdong here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Marcusdingdong{
+	private static Scanner in;
+	public static void main(String[] args) {
+		System.out.println("wow");
+}
+}
+		

ae937d31aea722de32a651c5674dd9fb9f0d23f1
ArchitHKIS <190902@hkis.edu.hk>
2016-09-01 09:43:29 +0800

    Init
    Init Project

---
+++ src/potato.java
@@ -0,0 +1,18 @@
+
+public class potato {
+	private static potato myPotato;
+	public potato(String string) {
+	}
+
+		
+
+	   public static void main(String []args){
+	      myPotato = new potato("potato");
+	   }
+	   public void potato1(String name){
+		      // This constructor has one parameter, name.
+		      System.out.println("Potato :" + name ); 
+		   }
+		   
+	
+}
